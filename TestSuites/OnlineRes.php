ecd3135dabb0754ae1364526cb217c37eb6fb9c5
zhjytest <nmgjiang304@163.com>
2017-11-02 08:45:39 +0800

    onlineres.php

--- TestSuites/OnlineRes.php
+++ TestSuites/OnlineRes.php
@@ -7,21 +7,21 @@ class OnlineRes extends PHPUnit_Framework_TestSuite{
 	public function __construct(){
 		$this->addTestFile('TestCases/AAArandstrTest.php');
 		$this->addTestFile('TestCases/AALoginTest.php');
-		// $this->addTestFile('TestCases/YzBCarTest.php');
-		// $this->addTestFile('TestCases/CarTest.php');
-		// $this->addTestFile('TestCases/YzActivityCarTest.php');
-		// $this->addTestFile('TestCases/YzACarTest.php');
-		// $this->addTestFile('TestCases/CustomerInfoTest.php');
-		// $this->addTestFile('TestCases/InvoiceTest.php');
+		$this->addTestFile('TestCases/YzBCarTest.php');
+		$this->addTestFile('TestCases/CarTest.php');
+		$this->addTestFile('TestCases/YzActivityCarTest.php');
+		$this->addTestFile('TestCases/YzACarTest.php');
+		$this->addTestFile('TestCases/CustomerInfoTest.php');
+		$this->addTestFile('TestCases/InvoiceTest.php');
 		$this->addTestFile('TestCases/ChangeInfoTest.php');
-		// $this->addTestFile('TestCases/AccidentTest.php');
-		// $this->addTestFile('TestCases/ActivitycurTest.php');
-		// $this->addTestFile('TestCases/ActivityListTest.php');
-		// $this->addTestFile('TestCases/ShareContentsTest.php');
-		// $this->addTestFile('TestCases/CodeuseTest.php');
-		// $this->addTestFile('TestCases/BreakDownTest.php');
-		// $this->addTestFile('TestCases/FeedBackTest.php');
-		// $this->addTestFile('TestCases/ActivitycurTest.php');
+		$this->addTestFile('TestCases/AccidentTest.php');
+		$this->addTestFile('TestCases/ActivitycurTest.php');
+		$this->addTestFile('TestCases/ActivityListTest.php');
+		$this->addTestFile('TestCases/ShareContentsTest.php');
+		$this->addTestFile('TestCases/CodeuseTest.php');
+		$this->addTestFile('TestCases/BreakDownTest.php');
+		$this->addTestFile('TestCases/FeedBackTest.php');
+		$this->addTestFile('TestCases/ActivitycurTest.php');
 	}
 
 	public static function suite(){
